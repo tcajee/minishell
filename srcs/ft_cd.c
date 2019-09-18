8827ec67bba3dbaa0283be4b2d47955663cb75a3
Tameem CAJEE <tcajee@c5r3s4.wethinkcode.co.za>
2019-09-18 16:22:17 +0200

    improved ft_cd

--- srcs/ft_cd.c
+++ srcs/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: mbaloyi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/10 16:12:18 by mbaloyi           #+#    #+#             */
-/*   Updated: 2019/09/18 16:09:10 by tcajee           ###   ########.fr       */
+/*   Updated: 2019/09/18 16:22:02 by tcajee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,16 @@ static char *ft_home(char **env)
 	i = 0;
 	while(env[i] != NULL)
 	{
-		home = ft_strsplit(env[i], '=');
+		home = ft_strsplit(env[i++], '=');
 		if (ft_strcmp(home[0], "HOME") == 0)
+		{
+			free(home[0]);
 			return(home[1]);
-		i++;
+		}
+		else
+			ft_tabfree(home);
 	}
-	return NULL;
+	return (NULL);
 }
 
 static char *ft_oldpwd(char **env)
@@ -36,12 +40,16 @@ static char *ft_oldpwd(char **env)
 	i = 0;
 	while(env[i] != NULL)
 	{
-		home = ft_strsplit(env[i], '=');
+		home = ft_strsplit(env[i++], '=');
 		if (ft_strcmp(home[0], "OLDPWD") == 0)
+		{
+			free(home[0]);
 			return(home[1]);
-		i++;
+		}
+		else
+			ft_tabfree(home);
 	}
-	return NULL;
+	return (NULL);
 }
 
 int			ft_is_dir(char *dir)
@@ -60,7 +68,7 @@ int			ft_is_dir(char *dir)
 	}
 	else if ((s_is.st_mode & S_IFMT) == S_IFDIR)
 	{
-		if ((!(s_is.st_mode & S_IRGRP) || !(s_is.st_mode & S_IROTH)))
+		if ((!(s_is.st_mode & S_IRGRP) && !(s_is.st_mode & S_IRUSR) && !(s_is.st_mode & S_IROTH)))
 		{
 			/* ft_printf("minishell: cd: %s: Permission denied\n", dir); */
 			printf("minishell: cd: %s: Permission denied\n", dir);
@@ -76,32 +84,32 @@ void	do_cd(char **arg, char **env)
 	char	*new;
 	char	*old;
 	char	path[PATH_MAX];
+	char *temp;
 
+	temp = NULL;
 	old = NULL;
 	old = getcwd(old, sizeof(old));
+
 	printf("old: %s\n", old);
 
-	if (!arg[1] || (ft_strcmp(arg[1], "--") == 0) ||
-		(ft_strcmp(arg[1], "$HOME" ) == 0))
-		chdir(ft_home(env));
-	else if ((ft_strcmp(arg[1], "-") == 0) ||
-		(ft_strcmp(arg[1], "$OLDPWD" ) == 0))
-		chdir(ft_oldpwd(env));
+	if (!arg[1] || (ft_strcmp(arg[1], "--") == 0) || (ft_strcmp(arg[1], "$HOME") == 0) || (ft_strcmp(arg[1], "~") == 0))
+		chdir(temp = ft_home(env));
+	else if ((ft_strcmp(arg[1], "-") == 0) || (ft_strcmp(arg[1], "$OLDPWD") == 0))
+		chdir(temp = ft_oldpwd(env));
 	else if (ft_is_dir(arg[1]) == 2)
 	{
 		readlink(arg[1], ft_memset(path, 0, PATH_MAX), PATH_MAX);
-		char *temp;
-		temp = ft_strjoin(ft_strsub(arg[1], 0, 
-				(ft_strrchr(arg[1], '/') - arg[1] + 1 )), path);
+		temp = ft_strjoin(ft_strsub(arg[1], 0, (ft_strrchr(arg[1], '/') - arg[1] + 1 )), path);
 		chdir(temp);
-		free(temp);
 	}
 	else if (ft_is_dir(arg[1]) == 1)
 		chdir(arg[1]);
 	new = NULL;
 	new = getcwd(new, sizeof(new));
+
 	printf("new: %s\n", new);
+
+	free(temp);
 	free(new);
 	free(old);
-	
 }
