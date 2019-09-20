ee0c5f318e6b16e733928b55b10edab326f61692
Stefan KORAC <skorac@c5r5s10.wethinkcode.co.za>
2019-09-20 12:41:21 +0200

    fixed unsetenv segfaulting when no value is found

--- srcs/ft_unsetenv.c
+++ srcs/ft_unsetenv.c
@@ -44,7 +44,8 @@ void	ft_unsetenv(char **env, char *arg)
 
 	elem = ft_strcat(arg, "=");
 	len = ft_tablen(env);
-	ft_findremove(env, arg);
+	
+	if (ft_findremove(env, arg) == 0)
+		ft_putendl("Value not found");
 	ft_pushback(env, &len);
-	free(elem);
 }
